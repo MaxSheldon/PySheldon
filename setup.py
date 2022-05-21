bf9a1faf7dfa93e765d3fa850d0a7ffab5114ce3
maxsheldon <getdavid00@gmail.com>
2022-05-21 00:45:17 -0300

    Adicionados arquivos requirements.txt e setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PySheldon',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/MaxSheldon/PySheldon.git',
+    license='Mrmagicpie License',
+    author='David Santana',
+    author_email='getdavid00@gmail.com',
+    description='Pacote com classes essenciais'
+)

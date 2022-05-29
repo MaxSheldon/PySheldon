72ddd3e9b5707934ed293915f6d6c82be75860fa
maxsheldon <getdavid00@gmail.com>
2022-05-29 00:45:21 -0300

    Log.py file updated

--- sample/log/log.py
+++ sample/log/log.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import os
 import sys
 import logging
+from os import path
 from datetime import datetime
 
 class LogLevel:
@@ -13,15 +15,29 @@ class LogLevel:
     CRITICAL = logging.CRITICAL
 
 class Log:
-    def __init__(self, file_name, print_log=False, log_format=None, log_level=logging.INFO):
+    def __init__(self,
+                 file_name : str,
+                 file_path : str = None,
+                 print_log : bool = False,
+                 log_format : str = None,
+                 log_level : LogLevel = logging.INFO
+                 ):
         if log_format is None:
             self.LOG_FORMAT = '%(asctime)s | %(levelname)s | %(funcName)s | %(message)s'
-        self.FILE_NAME = file_name
+        self.cria_file(file_name=file_name, file_path=file_path)
         self.PRINT_LOG = print_log
         self.LOG_LEVEL = log_level
         self.set_log()
 
-    def set_log(self):
+    def cria_file(self, file_path : str, file_name : str) -> None:
+        if file_path:
+            if path.exists(path=file_path) is False:
+                os.makedirs(file_path)
+            self.FILE_NAME = path.join(file_path, file_name)
+            return
+        self.FILE_NAME = '{}'.format(file_name)
+
+    def set_log(self) -> None:
         self.logger = logging.getLogger()
         self.logger.setLevel(level=self.LOG_LEVEL)
 
@@ -43,5 +59,5 @@ class Log:
         if self.PRINT_LOG: self.logger.addHandler(stdout_handler)
 
     @staticmethod
-    def get_date():
+    def get_date() -> str:
         return datetime.now().strftime('%Y-%d-%m')
